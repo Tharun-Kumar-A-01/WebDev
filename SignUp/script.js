73d65214a6f0e03a289c6091e8354f585b18bcc9
Tharun <tk22156simp@gmail.com>
2023-09-26 18:00:48 +0530

    Initilized and added files

---
+++ SignUp/script.js
@@ -0,0 +1,9 @@
+
+function Onclc () {
+
+	let username = document.getElementById('username').value
+	let email = document.getElementById('email').value
+	let password = document.getElementById('password').value
+	console.log(username,'\n',email,'\n',password)
+
+}
