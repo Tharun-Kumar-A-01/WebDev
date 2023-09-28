d1e6a205ce4c29e9f0c514c8846e498cfe84d706
Tharun <tk22156simp@gmail.com>
2023-09-28 16:04:31 +0530

    added porfolio and about page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    colors: {
+        'cyan':{
+            400 : '#22d3ee',
+            500 : '#06b6d4',
+        },
+    },
     extend: {},
   },
   plugins: [],
